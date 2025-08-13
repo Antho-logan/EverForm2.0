f196432610c48063df9f7a602af052dc4a1aa338
Antho-logan <anthonylogan1995@gmail.com>
2025-08-13 16:45:32 +0200

    Initial Commit

---
+++ EverForm/EverFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverFormApp.swift
+//  EverForm
+//
+//  Created by Anthony Logan on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EverFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
